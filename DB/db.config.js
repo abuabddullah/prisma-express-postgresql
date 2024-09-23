7beb16daf971c4843529c0cc01954037519c4eec
asifaowadud <asifaowadud@gmail.com>
2024-09-23 22:16:40 +0600

    basic-crud-prisma-express-postgresql

--- DB/db.config.js
+++ DB/db.config.js
@@ -1,7 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient({
-  // log: ["query"], // can be use ["query", "info", "warn", "error"] which will print on terminal
   log: ["query", "info", "warn", "error"], // can be use ["query", "info", "warn", "error"] which will print on terminal
 });
 
